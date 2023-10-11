11d53b94d650029861d525fe2d34f1d8bcaea0dd
thomasryu <thomas.sameshima@gmail.com>
2023-10-11 14:47:20 -0300

    Adding temporary SEO files

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next'
+
+// Dynamically generated robots.txt
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'https://acme.com/sitemap.xml',
+  }
+}
