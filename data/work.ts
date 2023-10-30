f4a7bc6dc65d51297ed74be4d4b31e9a76d62ca5
thomasryu <thomas.sameshima@gmail.com>
2023-10-30 02:01:17 -0300

    Updating projects

--- data/work.ts
+++ data/work.ts
@@ -98,10 +98,10 @@ export const projects: Project[] = [
     ],
 
     description:
-      "At first glance, including this portfolio in its own projects section may seem like a stretch, a desperate move even. However, because it's not a simple static webpage, I felt it worth including it here. For example, some technologies used in it were:",
+      "At first glance, including this portfolio in its own projects section may seem like a stretch, even desperate. However, because it's not a simple static web page, I felt it was worth including it. For example, some technologies used in it were:",
     items: [
       'Tailwind CSS: My first experience with the framework, it significantly accelerated the coding process',
-      "Contentful Headless CMS: Used as a data source for the articles' contents, the gallery page",
+      "Contentful Headless CMS: Used as a data source for the articles' contents and gallery page",
     ],
 
     tags: [
